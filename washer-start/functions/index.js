cadf4b08beebca9e4fec6ffa0a98c36e9265860e
yagyanshbhatia <yagyansh1999@gmail.com>
2019-03-15 17:06:22 +0530

    add execute intent

--- washer-start/functions/index.js
+++ washer-start/functions/index.js
@@ -53,7 +53,7 @@ exports.faketoken = functions.https.onRequest((request, response) => {
       access_token: '123access',
       expires_in: secondsInDay,
     };
-  }
+  }washer
   response.status(HTTP_STATUS_OK)
     .json(obj);
 });
@@ -117,8 +117,52 @@ app.onQuery((body) => {
 });
 
 app.onExecute((body) => {
-  // TODO: Implement EXECUTE response
-  return {};
+  const {requestId} = body;
+  const payload = {
+    commands: [{
+      ids: [],
+      status: 'SUCCESS',
+      states: {
+        online: true,
+      },
+    }],
+  };
+  for (const input of body.inputs) {
+    for (const command of input.payload.commands) {
+      for (const device of command.devices) {
+        const deviceId = device.id;
+        payload.commands[0].ids.push(deviceId);
+        for (const execution of command.execution) {
+          const execCommand = execution.command;
+          const {params} = execution;
+          switch (execCommand) {
+            case 'action.devices.commands.OnOff':
+              firebaseRef.child(deviceId).child('OnOff').update({
+                on: params.on,
+              });
+              payload.commands[0].states.on = params.on;
+              break;
+            case 'action.devices.commands.StartStop':
+              firebaseRef.child(deviceId).child('StartStop').update({
+                isRunning: params.start,
+              });
+              payload.commands[0].states.isRunning = params.start;
+              break;
+            case 'action.devices.commands.PauseUnpause':
+              firebaseRef.child(deviceId).child('StartStop').update({
+                isPaused: params.pause,
+              });
+              payload.commands[0].states.isPaused = params.pause;
+              break;
+          }
+        }
+      }
+    }
+  }
+  return {
+    requestId: requestId,
+    payload: payload,
+  };
 });
 
 exports.smarthome = functions.https.onRequest(app);
